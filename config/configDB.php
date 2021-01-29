aaedf76a5d96fee6a860b24bf8f7646b44f3066a
Cristina Nuñez <cristina.nunseb@educa.jcyl.es>
2021-01-29 08:48:36 +0100

    Update configBD.php

--- config/configDB.php
+++ config/configDB.php
@@ -1,10 +1,10 @@
 <?php
     //CONFIGURACIÓN CASA
-    
+    /*
     define('DNS', 'mysql:host=192.168.1.215;dbname=DAW215DBProyectoTema5;charset=UTF8');//Dirección IP y nombre de la base de datos
     define('USER', 'usuarioDAW215DBProyectoTema5');//Nombre de usuario de la base de datos
     define('PASSWORD', 'P@ssw0rd');//Contraseña del usuario de la base de datos
-    
+    */
     
     //CONFIGURACIÓN ENTORNO DE DESARROLLO
     /*
@@ -14,9 +14,9 @@
     */
     
     //CONFIGURACIÓN 1&1
-    /*
+    
     define('DNS', 'mysql:host=db5000278682.hosting-data.io;dbname=dbs272026');//Dirección IP y nombre de la base de datos
     define('USER', 'dbu287777');//Nombre de usuario de la base de datos
     define('PASSWORD', 'Covid1234$');//Contraseña del usuario de la base de datos
-    */
+    
 ?>
